d35227baa022f595a563f1aa9e43bc36f4e995be
Leticia <lsf@protonmail.ch>
2020-05-24 22:28:00 +0200

    esercizio con handlebars

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	//handlebars
-	var source   = document.getElementById("cd-template").innerHTML;
+	var source   = $('#cd-template').html();
 	var template = Handlebars.compile(source);
     // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
 	$.ajax({
@@ -10,7 +10,7 @@ $(document).ready(function() {
         console.log(data);
         var disco = data.response
         // console.log(disco);
-        // Ciclare quindi i dischi e ottenuti
+        // Ciclare quindi i dischi
         for (var i = 0; i < disco.length; i++) {
             var discoCorrente = disco[i]
             console.log(discoCorrente);
@@ -23,20 +23,22 @@ $(document).ready(function() {
 			console.log(immagine);
 			console.log(cantante);
 			console.log(anno);
-        // e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
-        } // ciclo for
+	        // e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+			var context = {
+			'cd-titolo': titolo,
+			'cd-cantante': cantante,
+			'cd-anno': anno,
+			'cd-immagine': immagine
+		};//fine handlebars
+			var html = template(context);
+			//appendo le cose nel html e non sul script...
+			$('.cds-container.container').append(html);
+        } // fine ciclo for
         },
         'error': function(data){
             alert('error')
         }
-    });//ajax
-	var context = {
-  	'cd-titolo': "titolo",
-  	author: {
-    id: 47,
-    name: "Yehuda Katz"
-	}//handlebars
-};
-});//document
+    });//fine ajax
+});//fine document
 
 //BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal")
